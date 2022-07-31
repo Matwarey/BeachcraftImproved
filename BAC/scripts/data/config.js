841b3c6d18be634fcff5a5feadc232ce83fa2160
Matwarey <matthewbr106@gmail.com>
2022-07-31 14:37:39 -0400

    Might as well add BadPacket[6] on the way

--- BAC/scripts/data/config.js
+++ BAC/scripts/data/config.js
@@ -259,6 +259,11 @@ export default
             "enabled": true,
             "punishment": "none",
             "minVLbeforeBan": 0
+		},
+		"badpackets6": {
+            "enabled": true,
+            "punishment": "none",
+            "minVLbeforeBan": 0
         }
     },
     "itemLists": {
